307341ef1ef0a7f51ab282f90e8892acc163f09b
hus <husainkc2001@gmail.com>
2025-11-15 23:49:22 -0500

    fix secs

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Settings:
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://husainc:mdbatmpwd123@cluster0.4h4cmun.mongodb.net/?appName=Cluster0")
-    DB_NAME = os.getenv("DB_NAME", "agentic_modeller")
+    MONGO_URI = os.getenv("MONGO_URI")
+    DB_NAME = os.getenv("DB_NAME")
 
 settings = Settings()
